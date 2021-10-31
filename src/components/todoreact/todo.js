9f36228c75873f7cf0b62689c5b3e56cb6fe144d
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-10-31 20:40:58 +0545

    First commit

---
+++ src/components/todoreact/todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+export const todo = () => {
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                    <figure></>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default todo 
